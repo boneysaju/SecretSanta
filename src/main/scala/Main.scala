26826a38c55214e607e5428e716d60f51cda6ccc
Yordan Angelov <yordan.angelov@ten10.com>
2018-11-08 16:16:17 +0000

    added a partipant case class; updated test data; wrote an example function for loading info from the text file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,26 +4,35 @@ object Main extends App {
 
   println("How would you like to use the app?")
   println("Insert 1  for Uploading a CSV file and insert 2 for Entering participants manually")
-  val userChoice = StdIn.readLine("Enter text here: ").toInt
+  val userChoice = StdIn.readLine().toInt
 
   userChoice match {
-    case 1 => userChoosesCsv()
-    case 2 => userChoosesManualInput()
+    case 1 => loadParticipants()
+    case 2 => promptManualInput()
     case _ => println("That is an invalid input. Please type 1  for Uploading a CSV file and type 2 for Entering participants manually")
   }
 
-  def userChoosesCsv(): Unit = {
-    println("Please add all information to the CSV file")
+  def loadParticipants(): List[Participant] = {
+    println("Getting information from participants.txt...")
     val fileSource = io.Source.fromFile("participants.txt")
-    for (line <- fileSource.getLines.drop(1)) { //ignores the headers
-      println(line)
-      val Array(name, email, address) = line.split(",").map(_.trim) //splits each line using the comma as a separator and map removes any excess white space
-      println(s"$name $email $address")
+    val test = fileSource.getLines().toList
+//    for (line <- fileSource.getLines.drop(1)) { //ignores the headers
+//      println(line)
+//      val Array(name, email, address) = line.split(",").map(_.trim) //splits each line using the comma as a separator and map removes any excess white space
+//      println(s"$name $email $address")
+//    }
+//    fileSource.close
+    test.flatMap{ x ⇒
+      val l = x.split(",").map(_.trim)
+      if(l.length == 4) {
+        println("Getting participant with name " + l(0) + " and email " + l(1) + "...")
+        Some(Participant(l(0), l(1), l(2), l(3)))
+      }
+      else None
     }
-    fileSource.close
   }
 
-  def userChoosesManualInput(): Unit = {
+  def promptManualInput(): Unit = {
     println("How many participants are in your party?")
     val numberOfPeople = Console.readInt()
 
---
+++ src/main/scala/Participant.scala
@@ -0,0 +1,6 @@
+case class Participant(
+ name: String,
+ email: String,
+ house: String,
+ postcode: String
+)
