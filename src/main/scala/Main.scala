ca9020f94d8f42f45b9fb51e55ec26229c1fae1b
Sophie Still <sophie.still@digital.hmrc.gov.uk>
2018-11-12 15:46:54 +0000

    Group session - added while loop on taking participant info

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,17 +3,18 @@ import scala.io.StdIn
 object Main extends App {
 
   println("How would you like to use the app?")
-  println("Insert 1  for Uploading a CSV file and insert 2 for Entering participants manually")
-  val userChoice = StdIn.readLine("Enter text here: ").toInt
+  println("Insert 1 to use the participants.txt file or 2 to enter participants manually")
+  val userChoice = StdIn.readLine("Enter 1 or 2 here: ").toInt
+  var numberOfPeople: Int = 0
 
   userChoice match {
     case 1 => userChoosesCsv()
     case 2 => userChoosesManualInput()
-    case _ => println("That is an invalid input. Please type 1  for Uploading a CSV file and type 2 for Entering participants manually")
+    case _ => println("That is an invalid input. Please type 1 for Uploading a CSV file and type 2 to enter participants manually")
   }
 
   def userChoosesCsv(): Unit = {
-    println("Please add all information to the CSV file")
+    println("Please check that the following information is correct:")
     val fileSource = io.Source.fromFile("participants.txt")
     for (line <- fileSource.getLines.drop(1)) { //ignores the headers
       println(line)
@@ -21,22 +22,42 @@ object Main extends App {
       println(s"$name $email $address")
     }
     fileSource.close
+    println("Do you wish to proceed with the above information?")
+    //Y or N. If N, println("Please revise the participants.txt file and try again")
+    //If Y, continue
   }
 
   def userChoosesManualInput(): Unit = {
     println("How many participants are in your party?")
-    val numberOfPeople = Console.readInt()
-
+    numberOfPeople = Console.readInt()
     if (numberOfPeople < 5) {
       print("You need a minimum of 5 people to use this application")
+      userChoosesManualInput()
     }
     else {
-      // put in a for loop which does this n number of times
-      print("Enter a name: ")
-      print("Enter an email: ")
-      print("Enter an address: ")
+      userEntersParticipantsManually()
     }
   }
+
+  def userEntersParticipantsManually(): Unit = {
+    var n: Int = 0
+    while (n < numberOfPeople)
+    //add validation for fields
+      // field cannot be empty
+    { println(s"Add participant ${n+1}")
+      print("Enter a name: ")
+      Console.readLine()
+      print("Enter an email: ")
+      Console.readLine()
+      print("Enter an address: ")
+      Console.readLine()
+      n = n + 1
+      println(s"Participant $n added")
+    }
+  }
+  //after n users are added,  println(s"All participants have been added to the list")
+  // how to store fields
+
 
 }
 
