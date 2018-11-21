66f5217e3106995eea799acd68361ea8e573114c
Sophie Still <sophie.still@digital.hmrc.gov.uk>
2018-11-21 17:23:47 +0000

    Renamed list val

--- src/main/scala/Pairing.scala
+++ src/main/scala/Pairing.scala
@@ -1,15 +1,15 @@
 object Pairing {
 
   def pairPeopleUp(n: Int, santaList: List[(String, String, String)]): Unit = {
-    val aList = scala.util.Random.shuffle(santaList)
+    val shuffleList = scala.util.Random.shuffle(santaList)
     var x: Int = 0
     while (x < n) {
-    try{ val alist = List(aList(x), aList({x + 1}))
+    try{ val alist = List(shuffleList(x), shuffleList({x + 1}))
       println(alist) //debugging
       Mail.sendMail(alist)}
       catch {
         case _: Throwable => {
-          val blist = List(aList(x), aList({(x + 1) - n}))
+          val blist = List(shuffleList(x), shuffleList({(x + 1) - n}))
           println(blist)
           Mail.sendMail(blist)
         }
@@ -17,6 +17,4 @@ object Pairing {
       x += 1
     }
   }
-
-  //randomiseListOrder()
 }
